98a8da2cccd339dc6d2075ee5c331cf58100ae67
Diego Alonso Bueno Pastor <dabueno@ayesa.com>
2025-07-29 21:00:54 -0500

    cambios adapdatados a plantilla

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
+
 document.getElementById('menu-toggle').addEventListener('click', () => {
     document.getElementById('nav-links').classList.toggle('show');
+});
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    const navLinks = document.getElementById('nav-links');
+    if (navLinks.classList.contains('show')) {
+      navLinks.classList.remove('show');
+    }
   });
-  
+});
